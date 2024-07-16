1743fcd0a3601dc9920dfbc14418fbf79520a66d
sdilolle <stephdilolle@gmail.com>
2024-07-15 21:30:11 -0400

    updated thought.js

---
+++ models/thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    username: { type: String, required: true },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const reactionSchema = new Schema({
+  reactionId: { type: Schema.Types.ObjectId, default: () => new Schema.Types.ObjectId() },
+  reactionBody: { type: String, required: true },
+  username: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+// Create a virtual property `reactionCount` that retrieves the length of the thought's reactions array field on query
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// Initialize the Thought model
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
