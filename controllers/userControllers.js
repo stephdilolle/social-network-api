6362350e2e505e5e44454877292897a19958d165
sdilolle <stephdilolle@gmail.com>
2024-07-22 20:36:16 -0400

    made changes to controllers and models. model path should work now

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { User } = require('../models/user');
 
 // get all users 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -1,28 +1,32 @@
 const { Schema, model } = require('mongoose');
 const { Thought } = require('./thought.js');
 
-// Schema to create User model
-const userSchema = new Schema(
-  {
-    username: { type: String, required: true, unique: true, trim: true }, // Fixed unique, trimmed properties
-    email: { type: String, required: true, unique: true },
-    thoughts: [{ type: Schema.Types.ObjectId, ref: Thought }], // Reference to the Thought model
-    friends: [{ type: Schema.Types.ObjectId, ref: User }], // Self-reference to User model
-  },
-  {
-    toJSON: {
-      virtuals: true,
+// Define the user schema using a function
+const createUserSchema = () => {
+  const userSchema = new Schema(
+    {
+      username: { type: String, required: true, unique: true, trim: true },
+      email: { type: String, required: true, unique: true },
+      thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+      friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     },
-    id: false,
-  }
-);
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
 
-// Create a virtual property `friendCount` that retrieves the length of the user's friends array field on query
-userSchema.virtual('friendCount').get(function () {
-  return this.friends.length;
-});
+  // Create a virtual property `friendCount` that retrieves the length of the user's friends array field on query
+  userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+  return userSchema;
+};
 
 // Initialize the User model
-const User = model('User', userSchema);
+const User = model('User', createUserSchema());
 
 module.exports = User;
